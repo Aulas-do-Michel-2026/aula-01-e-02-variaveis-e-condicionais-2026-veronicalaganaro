3186f696121d853429659116b9f66970f7b5ff93
veronicalaganaro <veronicalaganaro@gmail.com>
2026-04-09 13:45:58 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,5 +1,5 @@
 peso_da_pessoa = float ( input("Digite o peso do paciente em kg: "))
 
-medicamento = peso_da_pessoa/2
+medicamento = peso_da_pessoa*2
 
 print(f"Resposta: \nMédia:{medicamento} mg")
