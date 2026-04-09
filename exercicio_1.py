6db166285004090779e529dde5c73f9a6f85107f
veronicalaganaro <veronicalaganaro@gmail.com>
2026-04-09 13:24:14 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,21 +1,7 @@
-"""
-#### Exercício 1
+n1 = float( input("Digite a primeira nota: "))
+n2 = float( input("Digite a segunda nota: "))
+n3 = float( input("Digite a terceira nota: "))
 
-Receba três notas (números decimais) de um aluno e imprima a média.
+Media = (n1 + n2 + n3)/3
 
-Exemplo:
-
-Digite a primeira nota:
-8.5
-Digite a segunda nota:
-7.0
-Digite a terceira nota:
-9.0
-
-Resposta:
-Média: 8.17
-
-Dica: Use inputs para receber os dados! 
-Lembre de converter ele para o tipo necessário!
-Print na tela com "print"
-"""
+print(f"Resposta:  \nMédia:{Media}")
