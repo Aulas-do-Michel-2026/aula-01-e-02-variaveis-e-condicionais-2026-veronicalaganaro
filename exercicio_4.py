2d58e15840e49d50b0527669aa1c0fdab941d027
veronicalaganaro <veronicalaganaro@gmail.com>
2026-04-23 19:41:15 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -1,10 +1,13 @@
 cromossomo = input ("Digite o cromossomo : "). strip()
-posição = int (input("Digite a posição: "))
+posicao = int (input("Digite a posição: "))
 
 inicio_brca1 = 41196312
 fim_brca1 = 41277500
 
-if cromossomo == "chr17" and inicio_brca1 <= posição <= fim_brca1:
- Resposta: "Sim"
+if cromossomo == "chr17" and inicio_brca1 <= posicao <= fim_brca1:
+ resposta = "Sim"
 else:
-Resposta: "Não"
+ resposta = "Não"
+ 
+print("\nResposta:")
+print(resposta)
